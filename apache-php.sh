d129dc6f123b7eeb90b033163aa0e10ed2eafe58
Maxime Guillod <maxime@proteck.ch>
2017-06-05 15:34:54 +0200

    step 2 done

--- apache-php.sh
+++ apache-php.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 docker build -t="apache-php" docker-images/apache-php/.
-echo "#################################################"
-echo "          192.168.99.100:8080 "
-echo "#################################################"
-
 start http://192.168.99.100:8080
-
 docker run -it -p 8080:80 apache-php
